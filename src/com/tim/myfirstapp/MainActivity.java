d6c96304f16cc637dbeb2f0b562cf3ddc3dde141
tims88 <timstansbury88@gmail.com>
2014-01-03 16:04:01 -0700

    Added code to make it print out the response it receives from the server

--- src/com/tim/myfirstapp/MainActivity.java
+++ src/com/tim/myfirstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tim.myfirstapp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -94,8 +96,14 @@ class SendMessageToServerTask extends AsyncTask<String, Void, String> {
 			Socket client = new Socket("192.168.1.103", 4444);
 			PrintWriter printwriter = new PrintWriter(client.getOutputStream(),true);
 			printwriter.write(params[0]);  //write the message to output stream
-
+			
 			printwriter.flush();
+			
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			System.out.println("Response received is : " + in.readLine());
+			
+			in.close();
 			printwriter.close();
 			client.close();   //closing the connection
 			
