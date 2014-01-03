d1ba47ae32898cd034a6bd97de83df92507c990c
tims88 <timstansbury88@gmail.com>
2014-01-03 13:18:52 -0700

    Changed the app into a simple networking app

--- src/com/tim/myfirstapp/MainActivity.java
+++ src/com/tim/myfirstapp/MainActivity.java
@@ -1,22 +1,41 @@
 package com.tim.myfirstapp;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+import android.telephony.SmsManager;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	IntentFilter intentFilter;
 
+	private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //---display the SMS received in the TextView---
+            TextView SMSes = (TextView) findViewById(R.id.textView1);
+            String text = SMSes.getText().toString();
+            text += "\n" + intent.getExtras().getString("sms");
+            SMSes.setText(text);
+        }
+    };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//---intent to filter for SMS messages received---
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SMS_RECEIVED_ACTION");
 	}
 
 	@Override
@@ -29,26 +48,37 @@ public class MainActivity extends Activity {
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
 	    // Do something in response to button
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		//Intent intent = new Intent(this, DisplayMessageActivity.class);
 		EditText editText = (EditText) findViewById(R.id.edit_message);
 		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
+		
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage("3035034184", null,message , null, null);
+		
+		TextView textHistory = (TextView) findViewById(R.id.textView1);
+		String text = textHistory.getText().toString();
+		text += "\nMe: " + message;
+		textHistory.setText(text);
+		
+//		Intent i = new
+//                Intent(android.content.Intent.ACTION_VIEW);
+//            i.putExtra("address", "3035034184");
+//            i.putExtra("sms_body", message);
+//            i.setType("vnd.android-dir/mms-sms");
+//            startActivity(i);
 	}
 	
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	// Handle presses on the action bar items
-    	switch (item.getItemId()) {
-	    	case R.id.action_search:
-	    		// Go to activity_search
-	    		Intent intent = new Intent(this, SearchActivity.class);
-	    		startActivity(intent);
-	    		return true;
-	    		
-	    	default:
-	    		return super.onOptionsItemSelected(item);
-    	}
+    protected void onResume() {
+        //---register the receiver---
+        registerReceiver(intentReceiver, intentFilter);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        //---unregister the receiver---
+        unregisterReceiver(intentReceiver);
+        super.onPause();
     }
 
 }
