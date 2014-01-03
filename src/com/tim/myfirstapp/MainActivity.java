044adfd6cb7ea026bdfdb4c9db155f6ab8a6a7c5
tims88 <timstansbury88@gmail.com>
2014-01-03 15:51:09 -0700

    Made it so that the app will sent the received text message to my computer using a Socket

--- src/com/tim/myfirstapp/MainActivity.java
+++ src/com/tim/myfirstapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.tim.myfirstapp;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.Menu;
@@ -21,10 +27,14 @@ public class MainActivity extends Activity {
         @Override
         public void onReceive(Context context, Intent intent) {
             //---display the SMS received in the TextView---
-            TextView SMSes = (TextView) findViewById(R.id.textView1);
-            String text = SMSes.getText().toString();
-            text += "\n" + intent.getExtras().getString("sms");
-            SMSes.setText(text);
+//            TextView SMSes = (TextView) findViewById(R.id.textView1);
+//            String text = SMSes.getText().toString();
+//            text += "\n" + intent.getExtras().getString("sms");
+//            SMSes.setText(text);
+        	
+        	// Get the message
+        	String receivedMessage = intent.getExtras().getString("sms");
+        	new SendMessageToServerTask().execute(receivedMessage);
         }
     };
 	
@@ -56,16 +66,8 @@ public class MainActivity extends Activity {
 		sms.sendTextMessage("3035034184", null,message , null, null);
 		
 		TextView textHistory = (TextView) findViewById(R.id.textView1);
-		String text = textHistory.getText().toString();
-		text += "\nMe: " + message;
-		textHistory.setText(text);
+		textHistory.setText("");
 		
-//		Intent i = new
-//                Intent(android.content.Intent.ACTION_VIEW);
-//            i.putExtra("address", "3035034184");
-//            i.putExtra("sms_body", message);
-//            i.setType("vnd.android-dir/mms-sms");
-//            startActivity(i);
 	}
 	
 	@Override
@@ -81,4 +83,33 @@ public class MainActivity extends Activity {
         super.onPause();
     }
 
+}
+
+class SendMessageToServerTask extends AsyncTask<String, Void, String> {
+
+	@Override
+	protected String doInBackground(String... params) {
+		
+		try {
+			Socket client = new Socket("192.168.1.103", 4444);
+			PrintWriter printwriter = new PrintWriter(client.getOutputStream(),true);
+			printwriter.write(params[0]);  //write the message to output stream
+
+			printwriter.flush();
+			printwriter.close();
+			client.close();   //closing the connection
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("UnknownHostException");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("IOException");
+		}
+		
+		return null;
+	}
+	
 }
